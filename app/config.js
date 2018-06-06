cf1d1f0e2086fa9d4fc92e81b5f1d71805be09d1
sonia.turcotte@gmail.com <sonia.turcotte@gmail.com>
2018-06-06 10:43:28 +0100

    Content updates from Mark Woods for lab day

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Dartford Charge crossing',
+  serviceName: 'Dartford Crossing charge',
 
   // Default port that prototype runs on
   port: '3000',
